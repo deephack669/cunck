3a77d15f833eea7a2ceef6445b655681d840a2fc
Arlliden <vodkabotle@gmail.com>
2020-03-17 15:42:50 +0100

    Updates

--- precache-manifest.ff71b1dc97d7550b23d8573c04e874c6.js
+++ precache-manifest.ff71b1dc97d7550b23d8573c04e874c6.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.8c97409f.js"
   },
   {
-    "revision": "49e0c0c3b78a941f29bc",
-    "url": "./static/js/main.49e0c0c3.chunk.js"
+    "revision": "e57dab83812ab68b7dff",
+    "url": "./static/js/main.e57dab83.chunk.js"
   },
   {
     "revision": "59826417d8abc840b5e9",
     "url": "./static/js/2.59826417.chunk.js"
   },
   {
-    "revision": "fe4323c0047efb158ef299d3ae7f4c46",
+    "revision": "221a56326f7c1647a7c4aae9d5e4961e",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1acabec4e5bebf79e24f716655caf2d5.js"
+  "./precache-manifest.ff71b1dc97d7550b23d8573c04e874c6.js"
 );
 
 workbox.clientsClaim();
