8d10c81e614cb095025fa1280284eb478011745b
Arlliden <vodkabotle@gmail.com>
2020-03-17 15:40:01 +0100

    Updates

--- precache-manifest.1acabec4e5bebf79e24f716655caf2d5.js
+++ precache-manifest.1acabec4e5bebf79e24f716655caf2d5.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.8c97409f.js"
   },
   {
-    "revision": "e074c07644c2c04d8bcf",
-    "url": "./static/js/main.e074c076.chunk.js"
+    "revision": "49e0c0c3b78a941f29bc",
+    "url": "./static/js/main.49e0c0c3.chunk.js"
   },
   {
     "revision": "59826417d8abc840b5e9",
     "url": "./static/js/2.59826417.chunk.js"
   },
   {
-    "revision": "110edd9b1e9610ae76568fa46502416f",
+    "revision": "fe4323c0047efb158ef299d3ae7f4c46",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3398cc9debb9bb0752d5b74ecce4f976.js"
+  "./precache-manifest.1acabec4e5bebf79e24f716655caf2d5.js"
 );
 
 workbox.clientsClaim();
